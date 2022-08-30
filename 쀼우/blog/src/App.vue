8fb5fa5acaea21a17bc2d4b32fe8df31ed7d585d
김람운 <cocori899@gmail.com>
2022-08-30 23:33:50 +0900

    [EDIT] blog

--- 쀼우/blog/src/App.vue
+++ 쀼우/blog/src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Blog</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav"></ul>
-      </div>
-    </div>
-  </nav>
-  <div class="container-fluid">
-    <p>블로그입니다.</p>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding-left : 20px; padding-right: 20px;">
+  <a class="navbar-brand" href="#">Blog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    
   </div>
-  <blog :informations="informations" />
+</nav>
+
+
+  <div class="container-fluid" style="margin-top: 20px; text-align: center;">
+    <p>블로그입니다.</p>
+    <blog :informations="informations" />
+  </div>
 </template>
 <informarions />
 
@@ -44,10 +49,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  
+
 }
 </style>
